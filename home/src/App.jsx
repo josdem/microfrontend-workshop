0a1ac18131ea20d746bee20a06642f8197d63bc2
josdem <joseluis.delacruz@gmail.com>
2023-06-14 08:35:17 -0400

    Adding tailwind instead css

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -5,11 +5,11 @@ import Button from "./button";
 import "./index.css";
 
 const App = () => (
-  <div className="container">
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: host</div>
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
+    <div>CCSS: Tailwind</div>
     <Button/>
   </div>
 );
