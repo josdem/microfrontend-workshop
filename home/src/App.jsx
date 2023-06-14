b8aa0be1ed4a71def4c34501441dfc751a517af7
josdem <joseluis.delacruz@gmail.com>
2023-06-14 17:27:17 -0400

    Adding header and footer

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -4,13 +4,18 @@ import Button from "./button";
 
 import "./index.css";
 
+import Header from "./Header";
+import Footer from "./Footer";
+
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
+    <Header/>
+    <div className="my-10">
+      Home Page Content
+    </div>
     <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
     <Button/>
+    <Footer/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
