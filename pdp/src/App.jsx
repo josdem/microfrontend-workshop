36dca77bf058477b37ff276e326ec47b16233e15
josdem <joseluis.delacruz@gmail.com>
2023-06-14 08:43:25 -0400

    Adding pdp microfrontend

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "./button";
 
-import "./index.css";
+import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
+    <div>Name: pdp</div>
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
     <div>CSS: Tailwind</div>
-    <Button/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
