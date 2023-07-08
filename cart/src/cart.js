98c38578d4053412e7f353945a7cb7702cf50d36
josdem <joseluis.delacruz@gmail.com>
2023-07-08 14:45:13 -0400

    Pointing server to localhost

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -1,17 +1,18 @@
 import { BehaviorSubject } from 'rxjs';
 
-const SERVER_URL = 'https://shopping.josdem.io/products/'
+const SERVER_URL = 'http://localhost:8085/login'
 
 export const jwt = new BehaviorSubject(null);
 
 export const login = (username, password) => 
-    fetch('https://shopping.josdem.io/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
+    fetch(SERVER_URL, {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: { 'Authorization': 'Basic am9zZGVtOjEyMzQ1Njc4' }
     })
     .then(response => response.json())
     .then(data => {
+        console.log("data: " + data);
         jwt.next(data.token);
         return data.token;
     }).catch(error => console.error(error));
