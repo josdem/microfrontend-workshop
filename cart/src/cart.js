82dddb5f56a4eea2d92210e712915e07c00f195b
josdem <joseluis.delacruz@gmail.com>
2023-07-11 16:20:40 -0400

    Adding get cart fetch function

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { BehaviorSubject } from 'rxjs';
 
-const SERVER_URL = 'https://shopping.josdem.io/login'
+const SERVER_URL = 'https://shopping.josdem.io'
 
 export const jwt = new BehaviorSubject(null);
+export const cart = new BehaviorSubject(null);
+
+export const getCart = () =>
+    fetch(`${SERVER_URL}/cart`, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${jwt.value}`,
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        cart.next(data);
+        return data;
+    })
 
 export const login = (username, password) => 
-    fetch(SERVER_URL, {
+    fetch(`${SERVER_URL}/login`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
