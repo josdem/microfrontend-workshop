be2055a21d47a21979f3a7b97ceb5cd4d581512a
josdem <joseluis.delacruz@gmail.com>
2023-07-13 18:10:58 -0400

    Adding cart

--- cart/src/MiniCart.jsx
+++ cart/src/MiniCart.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react";
 
-import { cart } from "./cart";
-let value;
+import { cart, clearCart } from "./cart";
 
 export default function MiniCart() {
     const [items, setItems] = useState(undefined);
@@ -9,11 +8,7 @@ export default function MiniCart() {
 
     useEffect(() => {
         console.log("Value from cart: " + JSON.stringify(cart));
-        cart.subscribe((val) => {
-            console.log("Minicart: " + JSON.stringify(val));
-            value = val;
-            setItems(value);
-        });
+        setItems(cart);
         
         return cart.subscribe(val => setItems(val));
     }, []);
@@ -24,10 +19,64 @@ export default function MiniCart() {
 
     return (
         <>
-        <span onClick={ () => setShowCart(!showCart)} id="showcart_span">
+          <span onClick={() => setShowCart(!showCart)} id="showcart_span">
             <i className="ri-shopping-cart-2-fill text-2xl" id="showcart"></i>
             {items.length}
-        </span>
+          </span>
+          {showCart && (
+            <>
+              <div
+                className="absolute p-5 border-4 border-blue-800 bg-white rounded-xl text-black"
+                style={{
+                  width: 300,
+                  top: "2rem",
+                  left: -250,
+                }}
+              >
+                <div
+                  className="grid gap-3 text-sm"
+                  style={{
+                    gridTemplateColumns: "1fr 3fr 10fr 2fr",
+                  }}
+                >
+                  {items.map((item) => (
+                    <React.Fragment key={item.sku}>
+                      <div>{item.name}</div>
+                      <div className="text-right">
+                        {item.price}
+                      </div>
+                    </React.Fragment>
+                  ))}
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div>
+                    <hr />
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex-grow">
+                    <button
+                      id="clearcart"
+                      className="bg-white border border-green-800 text-green-800 py-2 px-5 rounded-md text-sm"
+                      onClick={clearCart}
+                    >
+                      Clear Cart
+                    </button>
+                  </div>
+                  <div className="flex-end">
+                    <button
+                      className="bg-green-900 text-white py-2 px-5 rounded-md text-sm"
+                      onClick={clearCart}
+                    >
+                      Checkout
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </>
-    )
+      );
+
 }
