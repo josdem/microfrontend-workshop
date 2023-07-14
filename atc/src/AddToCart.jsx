a38442753392ab571b5855cb2d81c201abf43d12
josdem <joseluis.delacruz@gmail.com>
2023-07-14 15:36:53 -0400

    Adding place add to cart

---
+++ atc/src/AddToCart.jsx
@@ -0,0 +1,21 @@
+import { createEffect, createSignal, Show } from "solid-js"
+import { jwt, addToCart } from "cart/cart"
+
+export default ({id}) => {
+    const [leggedIn, setLoggedIn] = createSignal(false)
+
+    createEffect(() => {
+        return jwt.subscribe((val) => setLoggedIn(!!val))
+    })
+
+    return (
+        <Show when={loggedIn()}>
+          <button
+            onClick={() => addToCart(id)}
+            class="bg-red-900 text-white py-2 px-5 rounded-md text-sm mt-5"
+          >
+            Add To Cart
+          </button>
+        </Show>
+      );
+}
