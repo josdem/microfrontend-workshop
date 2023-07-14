dab1e15e72bbb6ce9fd0923a8586c164095660ed
josdem <joseluis.delacruz@gmail.com>
2023-07-14 15:24:18 -0400

    Adding add to cart mfe project

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -25,12 +25,33 @@ export default function CartContent() {
                   <div></div>
                   <div></div>
                   <div></div>
-                  <div>
+                  <div className="text-right" id="grand_total">
                     {currency.format(
                       items.reduce((a, v) => a + v.price, 0)
                     )}
                   </div>
             </div>
+            {items.length > 0 && (
+        <div className="flex mb-10">
+          <div className="flex-grow">
+            <button
+              id="clearcart"
+              className="bg-white border border-green-800 text-green-800 py-2 px-5 rounded-md text-sm"
+              onClick={clearCart}
+            >
+              Clear Cart
+            </button>
+          </div>
+          <div className="flex-end">
+            <button
+              className="bg-green-900 text-white py-2 px-5 rounded-md text-sm"
+              onClick={clearCart}
+            >
+              Checkout
+            </button>
+          </div>
+        </div>
+      )}
         </>
     );
 }
