305bf1e22351daf7737c0043777f4f51d14b5299
josdem <joseluis.delacruz@gmail.com>
2023-07-15 17:48:27 -0400

    Formatting cart content

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,37 +1,37 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import { cart, clearCart } from "cart/cart";
 import { currency } from "home/products";
 
 export default function CartContent() {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    useEffect(
-        () => cart.subscribe((value) => setItems(value ?? [])),
-        []
-      );
+  useEffect(
+    () => cart.subscribe((value) => setItems(value ?? [])),
+    []
+  );
 
-    return (
-        <>
-            <div className="my-10 grid grid-cols-4 gap-5">
-            {items.map((item) => (
-                    <React.Fragment key={item.sku}>
-                      <div>{item.name}</div>
-                      <div className="text-right">
-                        {currency.format(item.price)}
-                      </div>
-                    </React.Fragment>
-                  ))}
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div className="text-right" id="grand_total">
-                    {currency.format(
-                      items.reduce((a, v) => a + v.price, 0)
-                    )}
-                  </div>
+  return (
+    <>
+      <div className="my-10 grid grid-cols-4 gap-5">
+        {items.map((item) => (
+          <React.Fragment key={item.sku}>
+            <div>{item.name}</div>
+            <div className="text-right">
+              {currency.format(item.price)}
             </div>
-            {items.length > 0 && (
+          </React.Fragment>
+        ))}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="text-right" id="grand_total">
+          {currency.format(
+            items.reduce((a, v) => a + v.price, 0)
+          )}
+        </div>
+      </div>
+      {items.length > 0 && (
         <div className="flex mb-10">
           <div className="flex-grow">
             <button
@@ -52,7 +52,6 @@ export default function CartContent() {
           </div>
         </div>
       )}
-        </>
-    );
+    </>
+  );
 }
-
