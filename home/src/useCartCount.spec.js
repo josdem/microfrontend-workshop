d8b36ea5100c92dcb631d6b0a7ec58ca296cf2f2
josdem <joseluis.delacruz@gmail.com>
2023-07-15 09:47:41 -0400

    Adding mock callback

--- home/src/useCartCount.spec.js
+++ home/src/useCartCount.spec.js
@@ -5,13 +5,25 @@ import { useCartCount } from './useCartCount';
 jest.mock('cart/cart', () => ({
     cart: {
         cartItems: [],
-        subscribe: jest.fn(),
+        subscribe: (cb) => {
+            callback = cb;
+        }
     },
 }));
+
+let callback = () => {};
 
 descibe('useCartCount', () => {
     it('should return the number of items in the cart', () => {
         const { result } = renderHook(() => useCartCount());
         expect(result.current).toBe(0);
-    }
+    })
+
+    it("should return cart count", () => {
+        const { result } = renderHook(() => useCartCount());
+        act(() => {
+            callback({cartIntems: [{sku:100}] });
+        });
+        expect(result.current).toBe(1);
+    })
 });
