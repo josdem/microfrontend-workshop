45d5a075c95da7062da8bcd4282a6d0c59a13044
josdem <joseluis.delacruz@gmail.com>
2023-07-15 18:07:58 -0400

    Formatting pdp content

--- pdp/src/PdpContent.jsx
+++ pdp/src/PdpContent.jsx
@@ -1,35 +1,38 @@
-import React,{ useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { getProductBySku, currency } from "home/products";
 import placeAddToCart from "atc/placeAddToCart";
 
 export default function PdpContent() {
-    const {sku} = useParams()
+    const { sku } = useParams()
     const [product, setProduct] = useState(null)
 
-    useEffect(() => {getProductBySku(sku).then(setProduct)}, [sku])
+    useEffect(() => { getProductBySku(sku).then(setProduct) }, [sku])
 
     const addToCart = useRef(null)
 
     useEffect(() => {
-        if(addToCart.current){
+        if (addToCart.current) {
             placeAddToCart(addToCart.current, product.sku)
         }
     }, [product])
 
-    if(!product) {
+    if (!product) {
         return null;
     }
 
     return (
-        <>
-        <div>
-            {product.name}
-            {currency.format(product.price)}
+        <div className="grid grid-cols-2 gap-5">
+          <div>
+            <div className="flex">
+              <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
+              <div className="font-bold text-3xl flex-end">
+                {currency.format(product.price)}
+              </div>
+            </div>
+            <div ref={addToCart}></div>
+          </div>
         </div>
-        <div ref={addToCart}></div>
-        </>
-    )
-
+      );
 
 }
