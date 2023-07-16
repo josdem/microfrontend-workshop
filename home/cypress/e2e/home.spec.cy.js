5adb7b418b8e1a843742a32091944aa651f2e2ca
josdem <joseluis.delacruz@gmail.com>
2023-07-16 16:24:18 -0400

    #1 Reading username and password from data

--- home/cypress/e2e/home.spec.cy.js
+++ home/cypress/e2e/home.spec.cy.js
@@ -2,19 +2,19 @@ describe("validating home page", () => {
 
     before(function () {
         cy.fixture("test").then((data) => {
-          this.data = data
+            this.data = data
         })
     })
 
     it("should add an item to the cart", function () {
         cy.visit(this.data.url)
         cy.get('#show-login').click()
-        cy.get('#username').type('josdem')
-        cy.get('#password').type('12345678')
+        cy.get('#username').type(this.data.username)
+        cy.get('#password').type(this.data.password)
         cy.get('#login-btn').click()
         cy.get('#add-to-cart-101').click()
         cy.get('#show-cart').click()
-        cy.get('#grand-total').should('have.text', '$1,260.00')
+        cy.get('#grand-total').should('have.text', this.data.grandTotal)
         cy.get('#clear-cart').click()
     });
 });
