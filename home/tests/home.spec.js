6710362caeb5c7a477a6a56009889ed1b880cbb0
josdem <joseluis.delacruz@gmail.com>
2023-07-16 21:57:08 -0400

    #2 Adding test from product description page

--- home/tests/home.spec.js
+++ home/tests/home.spec.js
@@ -14,3 +14,17 @@ test('should add an item to the cart', async ({ page }) => {
   await expect(page.locator('#grand-total')).toHaveText(properties.grandTotal)
   await page.locator('#clear-cart').click()
 })
+
+
+test('should add an item to the cart from product description page', async ({ page }) => {
+  await page.goto(properties.url)
+  await page.locator('#show-login').click()
+  await page.locator('#username').fill(properties.username)
+  await page.locator('#password').fill(properties.password)
+  await page.locator('#login-btn').click()
+  await page.locator('#product-101').click()
+  await page.locator('.bg-red-900').click()
+  await page.locator('#show-cart').click()
+  await expect(page.locator('#grand-total')).toHaveText(properties.grandTotal)
+  await page.locator('#clear-cart').click()
+})
