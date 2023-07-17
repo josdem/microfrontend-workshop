33b28c4e969e3aeca53cca398af86681e41abc03
josdem <joseluis.delacruz@gmail.com>
2023-07-16 21:53:40 -0400

    #2 Adding test properties file

--- home/tests/home.spec.js
+++ home/tests/home.spec.js
@@ -1,14 +1,16 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+const properties = require("./properties/test.properties")
+
 test('should add an item to the cart', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto(properties.url)
   await page.locator('#show-login').click()
-  await page.locator('#username').fill('josdem')
-  await page.locator('#password').fill('12345678')
+  await page.locator('#username').fill(properties.username)
+  await page.locator('#password').fill(properties.password)
   await page.locator('#login-btn').click()
   await page.locator('#add-to-cart-101').click()
   await page.locator('#show-cart').click()
-  await expect(page.locator('#grand-total')).toHaveText('$1,260.00')
+  await expect(page.locator('#grand-total')).toHaveText(properties.grandTotal)
   await page.locator('#clear-cart').click()
 })
---
+++ home/tests/properties/test.properties.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "url": "http://localhost:3000",
+    "username": "josdem",
+    "password": "12345678",
+    "grandTotal": "$1,260.00"
+}
