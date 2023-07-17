d3aff5f43601ccae56f274562395876da474cd7f
josdem <joseluis.delacruz@gmail.com>
2023-07-16 21:49:06 -0400

    Fixing clear cart id

--- cart/src/MiniCart.jsx
+++ cart/src/MiniCart.jsx
@@ -58,7 +58,7 @@ export default function MiniCart() {
             <div className="flex">
               <div className="flex-grow">
                 <button
-                  id="clearcart"
+                  id="clear-cart"
                   className="bg-white border border-green-800 text-green-800 py-2 px-5 rounded-md text-sm"
                   onClick={clearCart}
                 >
